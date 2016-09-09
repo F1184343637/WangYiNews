5caf6faf3a21db848efab80ca698bc75390f7613
wangdi <526046866@qq.com>
2016-09-09 20:55:51 +0800

    王迪 looper cell 创建  newsSB 更改

---
+++ Together/Together/WDLooperVC.m
@@ -0,0 +1,28 @@
+//
+//  WDLooperVC.m
+//  Together
+//
+//  Created by 王迪 on 16/9/9.
+//  Copyright © 2016年 刘宗飞. All rights reserved.
+//
+
+#import "WDLooperVC.h"
+
+@interface WDLooperVC ()
+
+@end
+
+@implementation WDLooperVC
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+
+
+
+@end
