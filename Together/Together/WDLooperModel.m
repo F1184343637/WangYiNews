1e463e340b19ad647ebf853164cbe7af0f48b1d9
wangdi <526046866@qq.com>
2016-09-09 21:27:07 +0800

    王迪 使用 tools 类

--- Together/Together/WDLooperModel.m
+++ Together/Together/WDLooperModel.m
@@ -7,6 +7,7 @@
 //
 
 #import "WDLooperModel.h"
+#import "CJHTTPSessionManager.h"
 
 @implementation WDLooperModel
 
@@ -28,31 +29,31 @@
 
 + (void)getModelArrayWithCompleteBlock:(void(^)(NSArray *array))comBlock andErrorBlock:(void(^)())errorBlock {
     
-//    HMNetworkManager *manager = [HMNetworkManager sharedManager];
+    CJHTTPSessionManager *manager = [CJHTTPSessionManager manager];
     
-//    [manager GET:@"http://c.m.163.com/nc/ad/headline/0-4.html" parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nonnull responseObject) {
-//    
-//        NSString *firstKey = responseObject.keyEnumerator.nextObject;
-//        
-//        NSArray *jsonArray = responseObject[firstKey];
-//        
-//        NSMutableArray *nmArr = [NSMutableArray array];
-//        
-//        [jsonArray enumerateObjectsUsingBlock:^(NSDictionary *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//            
-//            [nmArr addObject:[WDHeadModel modelWithDict:obj]];
-//        }];
-//        
-//        if (comBlock) {
-//            comBlock(nmArr.copy);
-//        }
-//        
-//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-//        
-//        if (errorBlock) {
-//            errorBlock();
-//        }
-//    }];
+    [manager GET:@"http://c.m.163.com/nc/ad/headline/0-4.html" parameters:nil success:^(NSURLSessionDataTask * _Nonnull task, NSDictionary *  _Nonnull responseObject) {
+    
+        NSString *firstKey = responseObject.keyEnumerator.nextObject;
+        
+        NSArray *jsonArray = responseObject[firstKey];
+        
+        NSMutableArray *nmArr = [NSMutableArray array];
+        
+        [jsonArray enumerateObjectsUsingBlock:^(NSDictionary *  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            
+            [nmArr addObject:[WDLooperModel modelWithDict:obj]];
+        }];
+        
+        if (comBlock) {
+            comBlock(nmArr.copy);
+        }
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        if (errorBlock) {
+            errorBlock();
+        }
+    }];
     
 }
 
