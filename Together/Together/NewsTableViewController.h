a4d82e0c1edc4e10e95f4619560e826691c81236
chengjian <chengjian@itcast.cn>
2016-09-09 20:05:26 +0800

    sb 创建

---
+++ Together/Together/NewsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewController.h
+//  Together
+//
+//  Created by 程建 on 16/9/9.
+//  Copyright © 2016年 刘宗飞. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewsTableViewController : UITableViewController
+
+@end
