acb186c8e4cca284a1e6895312caf9372e676aa7
chengjian <chengjian@itcast.cn>
2016-09-10 15:25:50 +0800

    Revert "Revert "王迪 定时器版本""
    This reverts commit 759250dc9318fa22c4b500f285d3f88739aa94f4.

--- Together/Together/WDLooperVC.m
+++ Together/Together/WDLooperVC.m
@@ -16,6 +16,10 @@
 
 @property (nonatomic ,strong) NSArray *dataArray;
 
+@property (nonatomic ,strong) NSTimer *timer;
+
+@property (nonatomic ,assign) NSInteger pageNum;
+
 @end
 
 @implementation WDLooperVC
@@ -37,6 +41,7 @@
     _flowLayout.minimumLineSpacing = 0;
     _flowLayout.itemSize = self.collectionView.bounds.size;
     _flowLayout.scrollDirection = UICollectionViewScrollDirectionHorizontal;
+    self.collectionView.showsHorizontalScrollIndicator = NO;
     self.collectionView.pagingEnabled = YES;
 }
 
@@ -48,16 +53,53 @@
        
         self.dataArray = array;
         
+        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:0 inSection:1] atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
+        
     } andErrorBlock:^{
         
         NSLog(@"wangdi -- error");
     }];
+    
+
+    [self addTimer];
+}
+
+- (void)addTimer {
+
+    _timer = [NSTimer scheduledTimerWithTimeInterval:1 target:self selector:@selector(scrollPerSec) userInfo:nil repeats:YES];
+    
+}
+
+- (void)deleteTimer {
+
+    [_timer invalidate];
+    _timer = nil;
+}
+
+- (void)scrollPerSec {
+    
+    NSIndexPath *indexPath ;
+    
+    if (_pageNum == 7) {
+        
+        indexPath = [NSIndexPath indexPathForItem:0 inSection:1];
+    }else {
+    
+        indexPath = [NSIndexPath indexPathForItem:_pageNum % 4 + 1 inSection:1];
+    }
+
+    [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionNone animated:YES];
     
 }
 
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
 
     return self.dataArray.count;
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+
+    return 3;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -70,9 +112,32 @@
     
     cell.model = model;
     
-    
-
     return cell;
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+
+    _pageNum = scrollView.contentOffset.x / self.view.bounds.size.width;
+
+    NSIndexPath *indexPath = [NSIndexPath indexPathForItem:_pageNum % 4 inSection:1];
+    
+    [self.collectionView scrollToItemAtIndexPath:indexPath atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
+
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+
+    [self scrollViewDidEndDecelerating:scrollView];
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+
+    [self deleteTimer];
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+
+    [self addTimer];
 }
 
 
